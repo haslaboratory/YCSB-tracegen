314c6490956e8deb9f9ee71ee2bd48d1bfd2e510
MagnetSaki <magnetsaki@foxmail.com>
2021-07-14 10:58:10 +0800

    Update ycsb.sh

--- ycsb.sh
+++ ycsb.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 DIR=/tmp/ycsb
-rm ${DIR}/*
+if [ -d ${DIR} ]
+then
+    rm ${DIR}/*
+else
+    mkdir ${DIR}
+fi
 bin/ycsb load rocksdb -s -P workloads/workloadc -p rocksdb.dir=${DIR}
 mv test.txt warm.txt
 bin/ycsb run rocksdb -s -P workloads/workloadc -p rocksdb.dir=${DIR}
